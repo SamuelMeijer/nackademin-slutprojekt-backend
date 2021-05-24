84e4d8c4ec20e5eb36a4bb2473bc15ae42ed7d0f
Emma Olsson <emma.olsson22@learnet.se>
2021-05-24 13:19:12 +0200

    updated

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,68 +1,77 @@
 const mongoose = require('mongoose');
 const router = require('express').Router();
+
+// Importing models
 const User = require('../models/User');
+const Order = require('../models/Order');
 
-
-// Till senare
-const Order = require('../models/Order')
-
-
-// json web token
-const jwt = require('jsonwebtoken');
 
 // bcrypt
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
 // For customers
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
 
-    if(req.body.email)
-    // Kolla om email redan existerar
+    const checkEmail = await User.exists({ email: req.body.email })
+    console.log('orderID', order._id)
+
+    const orders = await Order.find({}).populate('orders')
+
+    
+    let orderHistory = ""
+            orders.forEach((order) => {
+                return order += order._id
+            })
+
+
+            console.log(orderHistory)
+
     bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
+
         if (err) {
             res.json(err)
         } else {
+   
             const newUser = new User({
 
                 _id: new mongoose.Types.ObjectId(),
                 email: req.body.email,
                 password: hash,
                 name: req.body.name,
-                // automatiskt att customer ska skrivas in, fråga Hans...
-                role: 'customer', 
+
+                // Sets 'customer' as default value to role.
+                role: 'customer',
                 adress: {
                     street: req.body.street,
                     zip: req.body.zip,
                     city: req.body.city
                 },
-                //importera orders här
-/*                 orderHistory: [{
-                    type: new mongoose.Types.ObjectId,
-                    ref: 'Order'
-                }
-                ] */
+                orderHistory: orderHistory
             })
 
-            // Sparar användaren
-            newUser.save((err) => {
-                if (err) {
-                    res.json(err)
-                } else {
-                    res.json(newUser)
-                }
-            })
+
+
+            if (checkEmail) {
+
+                res.status(409).send({ msg: 'Email already exists' })
+
+            } else {
+                console.log(checkEmail)
+                // Saves the user
+                newUser.save((err) => {
+                    if (err) {
+                        res.json(err)
+                    } else {
+                        res.status(201).json(newUser)
+                    }
+                })
+            }
+
         }
     })
 
 })
 
-
-// Hämtal alla användare
-router.get('/', async (req, res) => {
-    const users = await User.find({}).populate('user')
-    console.log(users)
-    res.json(users)
-})
 
 module.exports = router;
