5397ed97c682226aac478375babdf02de042f9f2
Emma Olsson <emma.olsson22@learnet.se>
2021-05-25 13:55:37 +0200

    updated and commented

--- models/user.js
+++ models/user.js
@@ -4,15 +4,32 @@ const mongoose = require('mongoose');
 const userSchema = mongoose.Schema({
 
     _id: mongoose.Schema.Types.ObjectId,
-    email: String, //{ type: String, required: true },
-    password: String,//{ type: String, required: true },
-    name: String,//{ type: String },
-    role: { type: String, enum: ['customer', 'admin'], default: 'customer' } ,
+    email: { 
+        type: String, 
+        required: true },
+    password: { 
+        type: String, 
+        required: true },
+    name: { 
+        type: String, 
+        required: true },
+    role: { 
+        type: String, 
+        // Enum limits the value of role to be either customer or adming
+        enum: ['customer', 'admin'], 
+        // Sets the default value of role to 'customer'
+        default: 'customer' } ,
 
     adress: {
-        street: String, //{ type: String, required: true },
-        zip: Number, //,{ type: Number, required: true },
-        city: String, //{ type: String, required: true }
+        street: { 
+            type: String, 
+            required: true },
+        zip: { 
+            type: Number, 
+            required: true },
+        city: { 
+            type: String, 
+            required: true }
     },
    orderHistory: [{
     type: mongoose.Schema.Types.ObjectId,
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -14,63 +14,65 @@ const saltRounds = 10;
 // Route for register a new customer
 router.post('/', async (req, res) => {
 
-   // Checks in User-document if the email inserted aldready exists in the database
-   const checkEmail = await User.exists({ email: req.body.email })
+    // Checks in User-document if the email inserted aldready exists in the database
+    const checkEmail = await User.exists({ email: req.body.email })
     console.log(checkEmail)
 
-    // Encrypts the password inserted by the user, adds saltRounds-variable and adds encrypted password to the database
-    bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
+    // If boolean 'checkEmail' is true (if email inserted already exists in database)
+    if (!checkEmail) {
 
+        // Encrypts the password inserted by the user, adds saltRounds-variable and adds encrypted password to the database
+        bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
 
-        if (err) {
-            res.json(err)
-        } else {
+            if (err) {
 
-            // Creates a new document out of the User-model
-            const newUser = new User({
-
-                // Sets the _id to an ObjectId created by mongoose
-                _id: new mongoose.Types.ObjectId(),
-
-                //req.body = accessing data from the server
-                email: req.body.email,
-                password: hash,
-                name: req.body.name,
-
-                // Enum limits the value of role to be either customer or adming
-                // Sets 'customer' as default value to role
-                role: 'customer',
-                adress: {
-                    street: req.body.street,
-                    zip: req.body.zip,
-                    city: req.body.city
-                },
-                orderHistory: []
-            })
-
-         
-
-            // If boolean 'checkEmail' is true (if email inserted already exists in database)
-            if (checkEmail) {
-
-                
-                res.status(409).send({ msg: 'Email already exists' })
-
+                res.json(err)
             } else {
 
+                // Creates a new document out of the User-model
+                const newUser = new User({
+
+                    // Sets the _id to an ObjectId created by mongoose
+                    _id: new mongoose.Types.ObjectId(),
+
+                    //req.body = accessing data from the server
+                    email: req.body.email,
+
+                    // Sets the password to hash from BCrypt
+                    password: hash,
+                    name: req.body.name,
+
+                    // Sets 'customer' as default value to role
+                    // req.body.role can change the role to admin från insomnia/postman
+                    role: req.body.role,
+                    adress: {
+                        street: req.body.street,
+                        zip: req.body.zip,
+                        city: req.body.city
+                    },
+                    // Sets the orderHistory to an empty array
+                    orderHistory: []
+                })
+
+                // Slänga in check av lösen?
+
+                // Saves the newUser-document to database with .save-method
                 newUser.save((err) => {
                     if (err) {
-                        console.log(err)
                         res.json(err)
                     } else {
-                        // res.status(201).json(newUser)
                         res.json(newUser)
                     }
                 })
-            }
 
-        }
-    })
+
+            }
+        })
+    } else {
+
+        res.status(409).send({ msg: `The email is already registered` })
+    }
+
 
 })
 
