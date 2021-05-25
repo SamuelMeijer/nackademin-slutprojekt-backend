6289e63a7947cd32b3e04e0b9f41a0e4d9e382b5
Samuel Meijer <Samuel.j.meijer@gmail.com>
2021-05-25 14:01:38 +0200

    Fixing duplicate import of export-router

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -1,5 +1,4 @@
-const express = require('express').Router(); 
-const router = express.Router(); 
+const router = require('express').Router(); 
 const mongoose = require('mongoose'); 
 
 const Order = require('../models/Order');
