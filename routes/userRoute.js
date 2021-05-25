1c2d755847c94d09fc315d048f35125e752dfb85
Emma Olsson <emma.olsson22@learnet.se>
2021-05-25 09:09:26 +0200

    added comments to

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,37 +1,44 @@
 const mongoose = require('mongoose');
 const router = require('express').Router();
 
-// Importing models
+// Importing User-model
 const User = require('../models/User');
-const Order = require('../models/Order');
 
-
-// bcrypt
+// Importing BCrypt-module
 const bcrypt = require('bcrypt');
+
+// Sets the value 10 to saltRounds 
+// - controls how much time is needed to calculate a single BCrypt hash
 const saltRounds = 10;
 
 // Route for register a new customer
 router.post('/', async (req, res) => {
 
-    // Checks in the User-model if the email inserted aldready exists
-    const checkEmail = await User.exists({ email: req.body.email })
-    
-    // Encrypts the 
+   // Checks in User-document if the email inserted aldready exists in the database
+   const checkEmail = await User.exists({ email: req.body.email })
+
+    // Encrypts the password inserted by the user, adds saltRounds-variable and adds encrypted password to the database
     bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
+
 
         if (err) {
             res.json(err)
         } else {
-   
+
+            // Creates a new document out of the User-model
             const newUser = new User({
 
+                // Sets the _id to an ObjectId created by mongoose
                 _id: new mongoose.Types.ObjectId(),
+
+                //req.body = accessing data from the server
                 email: req.body.email,
                 password: hash,
                 name: req.body.name,
 
-                // Sets 'customer' as default value to role.
-                role: 'customer',
+                // Enum limits the value of role to be either customer or adming
+                // Sets 'customer' as default value to role
+                role: 'customer'/* { type: String, enum: ['customer', 'admin'], default: 'customer' } */,
                 adress: {
                     street: req.body.street,
                     zip: req.body.zip,
@@ -40,18 +47,22 @@ router.post('/', async (req, res) => {
                 orderHistory: []
             })
 
+         
+
+            // If boolean 'checkEmail' is true (if email inserted already exists in database)
             if (checkEmail) {
 
+                console.log('error')
                 res.status(409).send({ msg: 'Email already exists' })
 
             } else {
-                console.log(checkEmail)
 
                 newUser.save((err) => {
                     if (err) {
                         res.json(err)
                     } else {
-                        res.status(201).json(newUser)
+                        // res.status(201).json(newUser)
+                        res.json(newUser)
                     }
                 })
             }
