113bafc19819490c9a1366ddd36e5ffd30abe06c
Emma Olsson <emma.olsson22@learnet.se>
2021-05-26 02:13:32 +0200

    test

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -4,8 +4,16 @@ const router = require('express').Router();
 // Importing User-model
 const User = require('../models/User');
 
-// Importing BCrypt-module
+// cookie parser - module?
+const cookieParser = require('cookie-parser');
+// Behövs för att kunna hämta req.cookies
+// Skapar middleware?
+router.use(cookieParser())
+// bcrypt middleware/module?
 const bcrypt = require('bcrypt');
+// json web token middleware/module?
+const jwt = require('jsonwebtoken');
+
 
 // Sets the value 10 to saltRounds 
 // - controls how much time is needed to calculate a single BCrypt hash
@@ -16,7 +24,7 @@ router.post('/', async (req, res) => {
 
     // Checks in User-document if the email inserted aldready exists in the database
     const checkEmail = await User.exists({ email: req.body.email })
-    
+
     console.log(checkEmail)
 
     // If boolean 'checkEmail' is true (if email inserted already exists in database)
@@ -53,16 +61,73 @@ router.post('/', async (req, res) => {
                     // Sets the orderHistory to an empty array
                     orderHistory: []
                 })
+                
 
                 // Saves the newUser-document to database with .save-method
-                newUser.save((err) => {
+                newUser.save(async (err) => {
                     if (err) {
                         res.json(err)
                     } else {
 
                         // Lägg till token
-                        res.status(201).json(newUser)
+                        // Return valid token for user to be singed in after registration
+                        console.log('before', newUser)
+
+                        if (newUser) {
+                            const payload = {
+                                iss: 'sinus',
+                                uid: newUser.id
+                            }
+
+                            const token = jwt.sign(payload, process.env.SECRET_AUTH, { expiresIn: '1h' })
+
+                            const authResp = {
+
+                                token: token,
+                                user: {
+                                    email: req.body.email,
+                                    name: req.body.name,
+                                    role: newUser.role,
+                                    adress: {
+                                        street: req.body.adress.email,
+                                        zip: req.body.adress.zip,
+                                        city: req.body.adress.city
+                                    }
+                                }
+                            }
+
+
+                                res.cookie('auth-login', authResp)
+                                console.log(authResp)
+                
+                            
+
+                        }
                     }
+                    // res.status(201).json(newUser)
+                    //console.log('after', newUser)
+
+                    const loginToken = req.cookies['auth-login']['token']
+
+                    jwt.verify(loginToken, process.env.SECRET_AUTH, (err, payload) => {
+                        if(err){
+                            res.json(err)
+                        } else {
+                            if (req.body.password == req.body.repeatPassword){
+          
+                                res.status(201).json({newUser, loginToken})
+                            } else {
+        
+        
+                                res.status(409).send('Passwords entered does not match')
+                            } 
+ 
+                        }
+                    })
+
+
+
+
                 })
             }
         })
