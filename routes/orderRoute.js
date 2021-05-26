95677c081d5feeeb4bc8d31b7a3e936abcfed0c2
Tara78 <70331037+Tara78@users.noreply.github.com>
2021-05-26 15:28:18 +0200

    comment has done

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -4,14 +4,17 @@ const mongoose = require('mongoose');
 const Order = require('../models/Order');
 const User = require('../models/User')
 const Product = require('../models/Product')
+
+// Authorization 
 const jwtAuthentication = require('../middleware/jwtAuthentication');
 
+// Cookie Parser
 const cookieParser = require('cookie-parser');
 router.use(cookieParser())
 
 
 // Post request 
-router.post('/', async (req, res) => {
+router.post('/', jwtAuthentication, async (req, res) => {
     /* ***** CALCULATING orderValue ***** */
     // TODO: Remove, testing with console.log
     console.log('REQ.BODY.ITEMS :', req.body.items)
@@ -26,9 +29,10 @@ router.post('/', async (req, res) => {
     // TODO: Remove, testing with console.log 
     console.log('calcOrderValue :', calcOrderValue);
 
-
+// Fetch data & using auth-token method by checking if the email adress is exist and it is correct
     const user = await User.findOne({ email: req.cookies['auth-token']["user"]["email"]})
-            
+
+//Create order by useing order models   
     const newOrder = await new Order({
         _id: new mongoose.Types.ObjectId(), 
         timeStamp : Date.now(), 
@@ -36,20 +40,22 @@ router.post('/', async (req, res) => {
         orderValue: calcOrderValue, 
         items: req.body.items
         })
-         
-    console.log(req.body)
-
-         // Save Order in database
-  newOrder.save((err) => {
-    // Error Handeling with if ,else method
+    
+// Save Order in to the database
+  newOrder.save(async(err) => {
+// Error handeling with if - else statement Method
     if(err) { 
+
+ // if the order doesn't exist user can not submit anything, else creat an order
         return res.status(417).send('Expectation Failed! ')
 
     }else{
-        console.log('Create an Order')
-      
+         
+// 1- Push the order to orderHistodry (orderHistory is exist in user account)
+//2- Save the orde by recognizig (_id), (in case, if u are an user and log-in befor ordering)
         if(user){
           user.orderHistory.push(newOrder._id); 
+          await User.findByIdAndUpdate(user._id, user)
         }
         res.json(newOrder)
     }
@@ -57,19 +63,25 @@ router.post('/', async (req, res) => {
 
 })
 
-// get method from database
+// Using Get method to get user from database
 router.get('/', jwtAuthentication, async (req, res) => {
-        const user = await User.findOne({ email: req.cookies['auth-token']["user"]["email"]})
-        // const user = await  User.findOne({ email: payload.uid.user.email}, {orderHistory: 1}).populate('orderHistory')
-        if (user.role == 'customer') {
-            const userOrders = await  User.findOne({ email: req.cookies['auth-token']["user"]["email"]}, {orderHistory: 1}).populate('orderHistory')
+
+// Find log-in user in the database, and email address which it saved in cookies (which it excit id auth-token)  by checking if the email adress is correct
+       const user = await User.findOne({ email: req.cookies['auth-token']["user"]["email"]})
+// if you are an customer and login u can see all orders.
+// if the email address is correct u are a customer and u are able to see all your orders
+if (user.role == 'customer') {
+            const userOrders = await User.findOne({ email: req.cookies['auth-token']["user"]["email"]}, 
+// if One order is exist in the orderHistory customer can connect, Populate method refer to ObjectId array in the documets. 
+            {orderHistory: 1}).populate('orderHistory')
+// customer can see his order
             res.status(200).json(userOrders.orderHistory)
-        } else if (user.role == 'admin'){
-            const adminOrders = await  Order.find({})
-            res.json(adminOrders)
+
+// if you are an admin first you have to log-in then you can see all orders!  
         } else {
-            res.send('Du måste vara inloggad för at kunna se')
-        }
+            const adminOrders = await Order.find({})
+            res.json(adminOrders)
+        } 
 })
 
 module.exports= router;
