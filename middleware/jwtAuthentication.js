a1c610e63278b5535bee88c99df336efa6538c9c
Samuel Meijer <Samuel.j.meijer@gmail.com>
2021-05-26 16:47:58 +0200

    Adding comments to authRoute and jwtAuthentication

--- middleware/jwtAuthentication.js
+++ middleware/jwtAuthentication.js
@@ -1,37 +1,26 @@
+// Importing JSON Web Token
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-    // Evalutes if admin-auth
+    // Evalutes if a cookie with the name 'auth-token' exists
     if (req.cookies['auth-token']) {
 
-        //const token = req.cookies['auth-token']
+        // const token = req.cookies['auth-token']
         const token = req.cookies['auth-token']['token']
 
+        // Evaluates if the provided token has the same password as environment variable 'SECRET_AUTH'
         jwt.verify(token, process.env.SECRET_AUTH, async (err, payload) => {
+            // Evalutates if an error occured with the validation
             if (err) {
                 res.json(err);
+            
+            // If no error occured with the validation
             } else {
+                // Continue with the next route
                 next();
-                //res.send('Du är en admin')
-                // vad som ska göras om man är admin
             };
         });
 
-    // Evalutes if customer-auth
-    /*  } else if (req.cookies['auth-token-customer']) {
-
-        const token = req.cookies['auth-token-customer']
-
-        jwt.verify(token, process.env.SECRET_CUSTOMER, async (err, payload) => {
-            if (err) {
-                res.json(err);
-            } else {
-                next();
-                // res.send('Du är kund')
-                // vad som ska göras om man är kund
-            }
-        });
-        */
     // If none of the cookies can be found
     } else {
         res.status(401).send({msg: 'Du måste var inloggad eller admin'})
